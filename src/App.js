584f73dd8f351589491303a01002cb8f3ae391f8
dakaras <dan.karagiannis@protonmail.ch>
2020-01-31 12:56:41 -0600

    created navbar component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Error from './pages/Error'
 import SingleRoom from './pages/SingleRoom'
 import Rooms from './pages/Rooms'
 import {Route, Switch} from 'react-router-dom'
-
+import Navbar from './components/Navbar'
 function App() {
   return (
     <>
+    <Navbar/>
     <Switch>
-    <Route exact path='/' component={Home}/>
-    <Route exact path='/rooms' component={Rooms}/>
-    <Route exact path='/rooms/:single' component={SingleRoom}/>
-    <Route component={Error}/>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/rooms' component={Rooms}/>
+      <Route exact path='/rooms/:single' component={SingleRoom}/>
+      <Route component={Error}/>
     </Switch>
     </>
   );
