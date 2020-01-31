e0f4109bcecb61920cfcc4138213131a3921f27d
dakaras <dan.karagiannis@protonmail.ch>
2020-01-31 11:37:10 -0600

    added react router route

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Home from './pages/Home'
 import Error from './pages/Error'
 import SingleRoom from './pages/SingleRoom'
 import Rooms from './pages/Rooms'
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <Home/>
-    <Rooms/>
-    <SingleRoom/>
-    <Error/>
+    <Switch>
+    <Route exact path='/' component={Home}/>
+    <Route exact path='/rooms' component={Rooms}/>
+    <Route exact path='/rooms/:single' component={SingleRoom}/>
+    <Route component={Error}/>
+    </Switch>
     </>
   );
 }
