981348087c0fcaf0237041e0279f441775183422
dakaras <dan.karagiannis@protonmail.ch>
2020-02-01 11:24:34 -0600

    created banner.js

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Hero from '../components/Hero'
 const Rooms = () => {
     return (
-        <div>
-            hello from rooms
-        </div>
+        <Hero hero='roomsHero'>
+
+        </Hero>
     )
 }
 export default Rooms
