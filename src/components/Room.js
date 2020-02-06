49f953aca54badebdf685b3da807a5ff3bdbd7c0
dakaras <dan.karagiannis@protonmail.ch>
2020-02-06 15:30:19 -0600

    fixed featured rooms bug

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import defaultImg from '../images/details-1.jpg'
 
-export default function Room() {
+export default function Room({room}) {
+    const {name, images, slug, price} = room
+    
     return (
-        <div>
-            hello from room
-        </div>
+        <article className='room'>
+            <div className='img-container'>
+                <img src={images[0] || defaultImg} alt='single-room' height="250" width="250"/>
+              <div className='price-top'>
+                <h6>${price}</h6>
+                <p>per night</p>
+            </div>
+            </div>
+            
+        </article>
     )
 }
