5682fd575e6cdd9ed68857debdf9db4269a54755
dakaras <dan.karagiannis@protonmail.ch>
2020-02-06 12:06:08 -0600

    added loading.js

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -3,8 +3,9 @@ import {RoomContext} from '../Context'
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext
     render() {
-        const value = this.context
-        console.log(value)
+        const {featuredRooms: rooms} = this.context
+        console.log(rooms);
+        
         return (
             <div>
                 hello from FeaturedRooms
---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import loadingGif from './images/gif/loading-gear.gif'
+export default function Loading() {
+    return (
+        <div className='loading'>
+            <h4>ROOMS DATA LOADING...</h4>
+            <img src={loadingGif} alt='loading'/>
+        </div>
+    )
+}
